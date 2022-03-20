2c460a08ca8fdb36660309f579715484fe1afa31
m4rchn <93707158+m4rchn@users.noreply.github.com>
2022-03-20 21:38:16 +0700

    wp2_marselino_1.5

---
+++ wp2_marselino_pert1.5/application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+      public function index()
+      {
+      echo "<h1>Perkenalkan</h1>";
+      echo"Nama saya Marselino Raja Putra Tambunan
+           Saya tingga di Cilebut
+           Saya suka coding";
+
+      }
+
+    }
+ 
