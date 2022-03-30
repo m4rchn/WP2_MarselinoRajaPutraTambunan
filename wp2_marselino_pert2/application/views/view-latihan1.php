3f7496410d3e1ff31e9ce3831d686c9a2ca55f5c
m4rchn <93707158+m4rchn@users.noreply.github.com>
2022-03-30 15:54:30 +0700

    wp2_marselino_pert2

---
+++ wp2_marselino_pert2/application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> latihan 2 </title>
+</head>
+<body>
+    Ini hasil code <hr>
+    Nilai 1 = <?= $nilai1 ?><br>
+    Nilai 2 = <?= $nilai2 ?><br>
+    Hasil penjumlahannya yaitu <?= $nilai1 . " + " . $nilai2 . " = " .$hasil; ?>
+</body>
+</html>
